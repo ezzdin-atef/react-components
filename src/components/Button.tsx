ca6a3389e2a9f0cb5e320e33616b80c902d36d4a
Ezzdin Atef <ezzdin1125@gmail.com>
2022-04-07 09:36:15 +0200

    upload

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Button: React.FC = () => {
-  return <button className="text-3xl font-bold underline">text</button>;
+  return (
+    <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400">
+      text
+    </button>
+  );
 };

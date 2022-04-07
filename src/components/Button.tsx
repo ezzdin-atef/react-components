139ca6410f70b5cbd100bd15dd5829e806ec83ef
Ezzdin Atef <ezzdin1125@gmail.com>
2022-04-07 16:23:58 +0200

    fix issue

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,91 @@
 import React from 'react';
 
-export const Button: React.FC = () => {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+  loading?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  loading,
+  className,
+  label,
+  children,
+  ...other
+}) => {
   return (
-    <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400">
-      text
+    <button
+      className={`${className} mr-2 mb-2 rounded-lg px-5 py-2.5 text-sm font-medium 
+      ${loading ? 'focus:ring-none disabled:opacity-70' : ''}`}
+      disabled={loading}
+      {...other}
+    >
+      {loading ? 'Loading...' : children ? children : label}
     </button>
   );
+};
+
+const Default: React.FC<ButtonProps> = ({ className, children, ...other }) => {
+  return (
+    <Button
+      className={`${className}bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300`}
+      {...other}
+    >
+      {children}
+    </Button>
+  );
+};
+
+const Green: React.FC<ButtonProps> = ({ className, children, ...other }) => {
+  return (
+    <Button
+      className={`${className}bg-green-700 text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300`}
+      {...other}
+    >
+      {children}
+    </Button>
+  );
+};
+
+const Yellow: React.FC<ButtonProps> = ({ className, children, ...other }) => {
+  return (
+    <Button
+      className={`${className}bg-yellow-400 text-white hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300`}
+      {...other}
+    >
+      {children}
+    </Button>
+  );
+};
+
+const Red: React.FC<ButtonProps> = ({ className, children, ...other }) => {
+  return (
+    <Button
+      className={`${className}bg-red-700 text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300`}
+      {...other}
+    >
+      {children}
+    </Button>
+  );
+};
+
+const Light: React.FC<ButtonProps> = ({ className, children, ...other }) => {
+  return (
+    <Button
+      className={`${className} bg-whit border border-gray-300 text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200`}
+      {...other}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default {
+  Default,
+  Green,
+  Yellow,
+  Red,
+  Light,
 };
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,8 +1,46 @@
-import { Button } from '../components/Button';
-import '../index.css';
+import { Story } from '@storybook/react';
+import Button, { ButtonProps } from '../components/Button';
 
 export default {
   title: 'Button',
   component: Button,
 };
-export const Primary = () => <Button></Button>;
+
+// const Template: Story<ButtonProps> = args => <Button.Default {...args} />;
+
+export const Default = () => <Button.Default>Default</Button.Default>;
+export const Loading = () => <Button.Default loading>Default</Button.Default>;
+export const Green = () => <Button.Green>Green</Button.Green>;
+export const Yellow = () => <Button.Yellow>Yellow</Button.Yellow>;
+export const Red = () => <Button.Red>Red</Button.Red>;
+export const Light = () => <Button.Light>Light</Button.Light>;
+
+// export const Loading = Template.bind({});
+// Loading.args = {
+//   label: 'Button',
+//   loading: true,
+// };
+
+// export const Light = Template.bind({});
+// Light.args = {
+//   label: 'Button',
+//   light: true,
+// };
+
+// export const Green = Template.bind({});
+// Green.args = {
+//   label: 'Button',
+//   green: true,
+// };
+
+// export const Red = Template.bind({});
+// Red.args = {
+//   label: 'Button',
+//   red: true,
+// };
+
+// export const Yellow = Template.bind({});
+// Yellow.args = {
+//   label: 'Button',
+//   yellow: true,
+// };

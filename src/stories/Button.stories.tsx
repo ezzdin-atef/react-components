92369c5c69c637ab5e57c046e5353e422d205eef
Ezzdin Atef <ezzdin1125@gmail.com>
2022-04-07 12:55:29 +0200

    add storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,8 @@
+import { Button } from '../components/Button';
+import '../index.css';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+export const Primary = () => <Button></Button>;

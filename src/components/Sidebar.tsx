2661ae1901124cf9912a1126f38b4a657a8f6626
Ezzdin Atef <ezzdin1125@gmail.com>
2022-04-10 13:54:22 +0200

    fix sidebar issue

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,9 +14,12 @@ export interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const Sidebar: React.FC<SidebarProps> = ({ list, logo }) => {
   return (
-    <aside className="w-64" aria-label="Sidebar">
-      <img src={logo} alt="logo" className="mb-3 h-12" />
-      <div className="overflow-y-auto rounded bg-gray-50 py-4 px-3 dark:bg-gray-800">
+    <aside
+      className="min-h-screen w-64 overflow-y-auto bg-gray-50 "
+      aria-label="Sidebar"
+    >
+      <img src={logo} alt="logo" className="mx-auto mb-3 h-12" />
+      <div className="py-4 px-3 dark:bg-gray-800">
         <ul className="space-y-2">
           {list.map((item, index) => (
             <li key={index}>
